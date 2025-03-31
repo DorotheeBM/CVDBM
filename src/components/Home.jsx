9339a16d2bfc6638c76cb1282933236d7f0a8747
Dorothee <braud.dorothee@gmail.com>
2025-03-31 11:45:40 +0200

    avec le on c'est mieux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => (
         ADA Tech School </a> à Nantes depuis septembre 2024, afin de me perfectionner dans la gestion de projets agiles et de développer mes compétences techniques en informatique.
         <br></br>
         J’ai <b>plus de 15 ans d’expérience internationale</b> en gestion de projet, management, coordination et suis spécialisée dans les nouvelles technologies, la traduction  et l’audiovisuel. 
-        Je pense qu’on guide mieux une équipe quand maîtrise les mêmes connaissances techniques.
+        Je pense qu’on guide mieux une équipe quand on maîtrise les mêmes connaissances techniques.
       C’est pourquoi j’ai commencé cette formation de développeuse chez ADA, pour perfectionner mes connaissances de développement afin de pouvoir prétendre dans un futur proche à des postes de PM ou PO dans la tech.
         </p>
   </section>
