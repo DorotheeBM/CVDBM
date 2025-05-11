31de681965aa413b5c09d31af550fc52d83a39e8
Dorothee <braud.dorothee@gmail.com>
2025-05-11 21:51:30 +0200

    ajout du menu burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import Photo from "./assets/photodetouree.png";
 import FR from "./assets/FR.png";
 import USA from "./assets/USA.png";
-import "../index.css";
 import { LangContext } from "../App";
+import "../index.css";
 
 function Header() {
   const { lang, setLang, translations } = useContext(LangContext);
-  // const [menuOpen, setMenuOpen] = unseState(false);
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header-container">
       <div className="header-top">
@@ -17,6 +21,13 @@ function Header() {
           <img src={Photo} alt="Dorothée Braud-Meignant" className="header-photo" />
           <h1 className="header-name">Dorothée Braud-Meignant</h1>
         </div>
+
+        {/* Bouton hamburger */}
+        <button className="burger" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        {/* Drapeaux */}
         <div className="language-buttons">
           <button onClick={() => setLang("fr")} title={translations.french_title}>
             <img src={FR} alt={translations.french_alt} className="flag-icon" />
@@ -27,13 +38,14 @@ function Header() {
         </div>
       </div>
 
-      <nav className="nav-links">
-        <Link to="/" className="nav-link">{translations.home}</Link>
-        <Link to="/experiences" className="nav-link">{translations.experience}</Link>
-        <Link to="/projets" className="nav-link">{translations.projects}</Link>
-        <Link to="/competences-techniques" className="nav-link">{translations.technical_skills}</Link>
-        <Link to="/competences-douces" className="nav-link">{translations.soft_skills}</Link>
-        <Link to="/diplomes" className="nav-link">{translations.diplomas}</Link>
+      {/* Navigation principale */}
+      <nav className={`nav-links ${menuOpen ? "show" : ""}`}>
+        <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.home}</Link>
+        <Link to="/experiences" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.experience}</Link>
+        <Link to="/projets" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.projects}</Link>
+        <Link to="/competences-techniques" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.technical_skills}</Link>
+        <Link to="/competences-douces" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.soft_skills}</Link>
+        <Link to="/diplomes" className="nav-link" onClick={() => setMenuOpen(false)}>{translations.diplomas}</Link>
       </nav>
     </header>
   );
